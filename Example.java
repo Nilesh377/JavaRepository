ed3781863ee07ba7b46a4a462c48ca09bcf26fc2
Nilesh377 <50625650+Nilesh377@users.noreply.github.com>
2020-05-28 16:34:11 +0530

    first version

--- Example.java
+++ Example.java
@@ -1,11 +1,7 @@
 public class Example
 {
-     public static void main(String[] args)
-    {
-        int number1 = 10;
-        int number2 = 5;
-         int sum = number1 + number2;
-    
-        System.out.println("sum is : "+sum);
-        }
+  public static void main(String args[])
+  {
+    System.out.println("Hello Java Developers");
+  }
 }
