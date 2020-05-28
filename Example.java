099ad5b0744623a6e6f39235362d17cf4d7fb395
Nilesh377 <50625650+Nilesh377@users.noreply.github.com>
2020-05-28 16:27:24 +0530

    commit

--- Example.java
+++ Example.java
@@ -4,11 +4,8 @@ public class Example
     {
         int number1 = 10;
         int number2 = 5;
- 
-        
-        int sum = number1 + number2;
- 
-        
+         int sum = number1 + number2;
+    
         System.out.println("sum is : "+sum);
         }
 }
